d6b336bc70ca8cb29ac72e0d58f35239c1e67623
yossh1-gaming <ktsy0524@gmail.com>
2026-05-02 16:05:46 +0900

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -2,8 +2,8 @@
 // supabase.js
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-export const SUPABASE_URL = "https://camhjokfxzzelqlirxir.supabase.co";
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNhbWhqb2tmeHp6ZWxxbGlyeGlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg4Mjc1MTYsImV4cCI6MjA2NDQwMzUxNn0.WpJ2AWmbgCWHKwxrP9EmqEO4CGT65OjQsW2YSJcVCwM";
+export const SUPABASE_URL = "https://lhluebakxcqbyejzsgmu.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxobHVlYmFreGNxYnllanpzZ211Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3MDA1MzgsImV4cCI6MjA5MzI3NjUzOH0.MxpXaqjdLT5RgP2sXx1rel0OFI87zAqAjHL6dOgJDvE";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
