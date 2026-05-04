e4ee01d8be8f5faa26e76cc6205363465b397541
yossh1-gaming <ktsy0524@gmail.com>
2026-05-04 23:33:11 +0900

    v2.0

--- js/supabase.js
+++ js/supabase.js
@@ -3,7 +3,7 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 export const SUPABASE_URL = "https://lhluebakxcqbyejzsgmu.supabase.co";
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxobHVlYmFreGNxYnllanpzZ211Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3MDA1MzgsImV4cCI6MjA5MzI3NjUzOH0.MxpXaqjdLT5RgP2sXx1rel0OFI87zAqAjHL6dOgJDvE";
+export const SUPABASE_ANON_KEY = "sb_publishable_w1x6L94Vi7uhut6xOghYnw_JTfk53rR";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
